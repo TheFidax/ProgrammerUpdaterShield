c585d78746d218c0fb7055677eb01eadb28a39d6
TheFidax <58255296+TheFidax@users.noreply.github.com>
2021-11-04 17:08:07 +0100

    Add files via upload

--- FirmWare/FirmWare.ino
+++ FirmWare/FirmWare.ino
@@ -1,5 +1,7 @@
 #include "Arduino.h"
 
+#define FIRMWARE_VERSION    001
+
 #define ISP_LED 6
 #define I2C_LED 5
 
@@ -9,30 +11,30 @@ typedef enum { ISP, I2C } Mode;
 Mode mode;
 
 void setup() {
-  pinMode(ISP_LED, OUTPUT);
-  pinMode(I2C_LED, OUTPUT);
-
-  if(digitalRead(I2C_PULLUP) == HIGH) {
-    mode = I2C;
-  }
-  else {
-    mode = ISP;
-  }
+    pinMode(ISP_LED, OUTPUT);
+    pinMode(I2C_LED, OUTPUT);
   
-  if (mode == ISP) {
-    digitalWrite(ISP_LED, HIGH);
-    setup_ArduinoISP();
-    while(1) {
-      loop_ArduinoISP();
+    if(digitalRead(I2C_PULLUP) == HIGH) {
+        mode = I2C;
     }
-  }
-  else if (mode == I2C) {
-    digitalWrite(I2C_LED, HIGH);
-    setup_ArduinoI2C();
-    while(1) {
-      loop_ArduinoI2C();
+    else {
+        mode = ISP;
     }
-  }
+    
+    if (mode == ISP) {
+        digitalWrite(ISP_LED, HIGH);
+        setup_ArduinoISP();
+        while(1) {
+            loop_ArduinoISP();
+        }
+    }
+    else if (mode == I2C) {
+        digitalWrite(I2C_LED, HIGH);
+        setup_ArduinoI2C();
+        while(1) {
+          loop_ArduinoI2C();
+        }
+    }
 }
 
 void loop() {}
